b8e0fad9cdfa523b02eebf734fe72b7359a90aba
kunalkumar1437 <156920655+kunalkumar1437@users.noreply.github.com>
2026-06-05 14:36:08 +0530

    Updated Razorpay API keys

--- learnify/settings.py
+++ learnify/settings.py
@@ -150,5 +150,5 @@ EMAIL_HOST_USER = 'ganeshplays700@gmail.com'
 EMAIL_HOST_PASSWORD = 'iadwfjyshkymzsnz'
 
 
-KEY_ID = "rzp_live_StIg1xIdGc7IFa"
-KEY_SECRET = "TLY0E48b5WGtYITlFOETwZSO" 
+KEY_ID = "rrzp_test_SxtHfkfHK7WR5N"
+KEY_SECRET = "nY9impVcLJVO5j4zzzgofpmp" 
